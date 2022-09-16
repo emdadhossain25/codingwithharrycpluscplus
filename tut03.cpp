ea2bea2ad521615e366f7324949e489a81b37ca5
emdad <emdad@ergo-ventures.com>
2022-09-17 02:38:28 +0600

    added single and multiline comment

--- tut03.cpp
+++ tut03.cpp
@@ -1,7 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-int main(){
-    cout<<"hello world!"<<endl;
+// this is a single line comment
+/*this
+is
+a multi
+line
+comment
+*/
+int main()
+{
+    cout << "hello world!" << endl;
     return 0;
 }
