84f2659da31170a4e554c6e596aa419bb3b938ed
emdad <emdad@ergo-ventures.com>
2022-09-17 21:03:34 +0600

    break and continue understood

--- tut11.cpp
+++ tut11.cpp
@@ -2,14 +2,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    //freopen("input.txt", "r", stdin);
-    //freopen("output.txt", "w", stdout);
-    for (int i = 0; i < 4; i++)
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    for (int i = 0; i < 40; i++)
     {
         /* code */
-        cout<<i<<endl;
+        if (i == 2)
+        {
+            break; // break out of the loop don execute further iteration (break for i==2)
+        }
+
+        if (i == 1)
+        {
+            continue; // skip below statements for this iteration (will skip for i==1)
+        }
+        // here this break and continue will only print for value of 0
+        cout << i << endl;
     }
-    
+
     return 0;
 }
