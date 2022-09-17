05604add417dd05a59fa546d15cda8da595c0f7a
emdad <emdad@ergo-ventures.com>
2022-09-17 20:58:00 +0600

    code snippet edite

---
+++ tut11.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    for (int i = 0; i < 4; i++)
+    {
+        /* code */
+        cout<<i<<endl;
+    }
+    
+    return 0;
+}
