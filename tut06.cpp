9d13267d9b3713e8825dd3932e9181eae47d1365
emdad <emdad@ergo-ventures.com>
2022-09-17 14:46:32 +0600

    operators in cpp

---
+++ tut06.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+// there are two types of header files
+// 1. system header files/compiler header files
+// 2. user defined header files which are created by programmer
+int main()
+{
+    int a = 4, b = 5;
+    cout << "Hello World!" << endl;
+    cout << "operators in c++" << endl;
+    cout << "following are the operators in c++" << endl;
+    cout << endl;
+
+    // arithmatic operator
+    cout << "following are the arithmatic operators in c++" << endl;
+    cout << "the value of a+b " << a + b << endl;
+    cout << "the value of a-b " << a - b << endl;
+    cout << "the value of a*b " << a * b << endl;
+    cout << "the value of a/b " << a / b << endl;
+    cout << "the value of a%b " << a % b << endl;
+    cout << "the value of a++ " << a++ << endl;
+    cout << "the value of a-- " << a-- << endl;
+    cout << "the value of ++a " << ++a << endl;
+    cout << "the value of --a " << --a << endl;
+    cout << endl;
+
+    // assignment operators
+    // int a=4;
+    // bool b =false;
+    // char c= 'i';
+
+    // comparison operators
+    cout << "following are the comparison operators in c++" << endl;
+    cout << "the value of a==b is: " << (a == b) << endl;
+    cout << "the value of a!=b is: " << (a != b) << endl;
+    cout << "the value of a>b is: " << (a > b) << endl;
+    cout << "the value of a<b is: " << (a < b) << endl;
+    cout << "the value of a<=b is: " << (a <= b) << endl;
+    cout << "the value of a>=b is: " << (a >= b) << endl;
+
+    return 0;
+}
