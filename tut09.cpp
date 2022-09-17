534f3ee3b92e35f75ddf86e5ca5c1c730ceef337
emdad <emdad@ergo-ventures.com>
2022-09-17 20:18:23 +0600

    control statements

--- tut09.cpp
+++ tut09.cpp
@@ -12,23 +12,45 @@ int main()
     cout << "tell me your age" << endl;
     cin >> age;
     cout << "Your age is " << age << endl;
-    if ((age < 18) && (age > 0))
+
+    // 1. selection control structure if else-if else ladder
+    // if ((age < 18) && (age > 0))
+    // {
+    //     cout << "You can not come to my party" << endl;
+    // }
+    // else if (age == 18)
+    // {
+    //     cout << "You are a kid and you will get a kid pass to the party" << endl;
+    // }
+    // else if (age < 1)
+    // {
+    //     cout << "You are not yet born" << endl;
+    // }
+
+    // else
+    // {
+    //     cout << "You can come to the party" << endl;
+    // }
+
+    // 2. selection control structure switch case statements
+    switch (age)
     {
-        cout << "You can not come to my party" << endl;
-    }
-    else if (age == 18)
-    {
-        cout << "You are a kid and you will get a kid pass to the party" << endl;
-    }
-    else if (age < 1)
-    {
-        cout << "You are not yet born" << endl;
+    case 18:
+        cout << "you are 18" << endl;
+        break;
+    case 2:
+        cout << "you are 2" << endl;
+        break;
+    case 22:
+        cout << "you are 22" << endl;
+        break;
+
+    default:
+        cout << "No Special Cases" << endl;
+        break;
     }
 
-    else
-    {
-        cout << "You can come to the party" << endl;
-    }
+    cout << "done with switch case" << endl;
 
     return 0;
 }
