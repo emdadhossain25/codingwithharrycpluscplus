d8e86e5b05c77ccf83df509f22eb6276a2007dc1
emdad <emdad@ergo-ventures.com>
2022-09-18 21:54:23 +0600

    function prototype defined

--- tut15_function_function_prototypes.cpp
+++ tut15_function_function_prototypes.cpp
@@ -2,11 +2,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int sum(int a, int b)
-{
-    int c = a + b;
-    return c;
-}
+// type functionname(arguments) --> prototype
+
+// int sum(int a, int b); --> acceptable
+// int sum(int num1,  num2); --> not acceptable
+int sum(int, int); //-->this is also acceptable
+
+// void g(void); //--> acceptable
+void g(); //--> acceptable
+
 int main()
 {
     // freopen("input.txt", "r", stdin);
@@ -17,7 +21,22 @@ int main()
     cin >> num1;
     cout << "Enter second number" << endl;
     cin >> num2;
-    cout << "The sum of " << num1 << " and " << num2 << " is: " << sum(num1, num2)<<endl;
 
+    // num1 and num2 are actual parameters
+    cout << "The sum of " << num1 << " and " << num2 << " is: " << sum(num1, num2) << endl;
+    g();
     return 0;
+}
+
+// declaring after the main function --> solution to this problem is function prototype (declaring function signature in front)
+int sum(int a, int b)
+{
+    // a and b are formal parameters
+    int c = a + b;
+    return c;
+}
+
+void g()
+{
+    cout << "Hello World!" << endl;
 }
